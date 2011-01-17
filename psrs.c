fed8af63875bff581e547031a0d5602efb1df1e3
Florian Pilz <fpilz87@googlemail.com>
2011-01-17 16:57:34 +0100

    deployment of random numbers should work with this scheme

--- psrs.c
+++ psrs.c
@@ -6,23 +6,27 @@ int main(int argc, char *argv[])
   int rank, size;
 
   MPI_Init(&argc, &argv);
-  MPI_Comm_rank (MPI_COMM_WORLD, &rank);
-  MPI_Comm_size (MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
   
   // Zufallszahlen erzeugen (mit seed)
   
   // Zufallszahlen gleichmäßig verteilen
   
-  int random_numbers[size * 100], numbers_per_processor[100];
+  int random_numbers[10], numbers_per_processor[5];
   int i;
-  for (i = 0; i < size * 100; i++) {
-    random_numbers[i] = size * 100 - i;
+  for (i = 0; i < 10; i++) {
+    random_numbers[i] = 10 - i - 1;
   }
   
+  printf("size: %d, rank: %d\n", size, rank);
+  
   // MPI_Scatter(sendbuf, sendcount, sendtype, recvbuf, recvcount, recvtype, root, comm)
-  MPI_Scatter(random_numbers, size * 100, MPI_INT, numbers_per_processor, 100, MPI_INT, MPI_COMM_WORLD);
+  // sendcount must be equal to recvcount
+  MPI_Scatter(&random_numbers, 5, MPI_INT, &numbers_per_processor, 5, MPI_INT, 0, MPI_COMM_WORLD);
+  
   printf("my rank is %d, my data is ", rank);
-  for (i = 0; i < 100; i++) {
+  for (i = 0; i < 5; i++) {
     printf("%d", numbers_per_processor[i]);
   }
   printf("\n\n");
